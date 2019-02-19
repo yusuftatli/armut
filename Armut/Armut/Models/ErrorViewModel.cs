ebf946640deba4a62e4227280f2f3b54bd98405a
aspcan <yctatli@gmail.com>
2019-02-19 20:28:21 +0300

    first commite

---
+++ Armut/Armut/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Armut.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
